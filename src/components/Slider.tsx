acfdaa6e5df97de603123bfe2d5906e1956b8eb0
aumxbrainbean <aum@brainbean.in>
2025-11-13 23:20:21 +0530

    Change slider link and image source

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -8,7 +8,7 @@ export default function Slider() {
 
   const handleClick = (e: React.MouseEvent) => {
     e.preventDefault()
-    router.push("/shop")
+    router.push("/brands/starmax")
   }
 
   return (
@@ -24,7 +24,7 @@ export default function Slider() {
         aria-label="View featured collection - click to shop"
       >
         <img
-          src="https://headlesswp.dev.brainbean.us/wp-content/uploads/2025/11/Brixz-Disposable-Slider.webp"
+          src="https://headlesswp.dev.brainbean.us/wp-content/uploads/2025/11/starmax-30k-V01.webp"
           alt="Featured collection banner"
           className="w-full h-80 sm:h-96 lg:h-[500px] object-cover group-hover:scale-105 transition-transform duration-500"
         />
@@ -32,3 +32,4 @@ export default function Slider() {
     </section>
   )
 }
+
